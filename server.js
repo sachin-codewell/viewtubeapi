854bbf6490de9a5d489cfcce8d73f84beac96196
Sachin Yadav <sny2771999@gmail.com>
2022-11-15 13:39:25 +0530

    process.en.PORT change

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express=require("express");
 const axios = require('axios');
 const app= express();
-const port = 5000;
 const apiKey = "AIzaSyC7sWeL1tIQ4ttR05-GiDKAL_OViIpN5M4";
 const baseApiUrl = "https://www.googleapis.com/youtube/v3";
+
+const port = process.env.PORT||5000
 
 const bodyParser = require('body-parser');
 const apiRoutes = require('./routes/apiRoutes');
@@ -17,7 +18,6 @@ app.use(cors());
 
 app.use(bodyParser.json());
 app.use('/api',apiRoutes);
-
 
 
 app.listen(port ,()=>{
